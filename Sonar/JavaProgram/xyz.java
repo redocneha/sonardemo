edcebb6e688dd2b7a3a2053da8671e843a162860
Neha Shaik <nehshaik1@publicisgroupe.net>
2019-07-10 08:56:22 +0530

    nh

---
+++ Sonar/JavaProgram/xyz.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class person
+{
+String name;
+int age;
+}
+public class Xyz
+{
+public static void main(String args[])
+{
+List l=new LinkedList();
+l.add(1);
+l.add(8);
+l.add(89);
+System.out.println(l);
+person p=new person();
+p.name="neha";
+p.age=20;
+}
+}
